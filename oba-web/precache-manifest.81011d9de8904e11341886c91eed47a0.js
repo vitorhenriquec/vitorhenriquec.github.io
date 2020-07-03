7acb3c8fe639887db7c6c054976e0885abbbf59f
Vitor Henrique <vitor.henrique@esig.com.br>
2020-07-03 20:24:22 -0300

    Adicionando obama-web

---
+++ oba-web/precache-manifest.81011d9de8904e11341886c91eed47a0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5d06985c9568ddc892f32bc6fad3878",
+    "url": "/oba-web/index.html"
+  },
+  {
+    "revision": "9e0069e682aac404f556",
+    "url": "/oba-web/static/css/main.f0c22664.chunk.css"
+  },
+  {
+    "revision": "a0a4686831f118998f56",
+    "url": "/oba-web/static/js/2.f2b44bd3.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/oba-web/static/js/2.f2b44bd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e0069e682aac404f556",
+    "url": "/oba-web/static/js/main.05b7a6fc.chunk.js"
+  },
+  {
+    "revision": "48f0e9ddbfe063281cec",
+    "url": "/oba-web/static/js/runtime-main.ae9ede6c.js"
+  },
+  {
+    "revision": "5cb1238a62af7511d40b519116a7c23f",
+    "url": "/oba-web/static/media/bg.5cb1238a.jpeg"
+  },
+  {
+    "revision": "fdcda7b4e9f9cdfa59670b617c828beb",
+    "url": "/oba-web/static/media/icone-usuario.fdcda7b4.svg"
+  }
+]);
